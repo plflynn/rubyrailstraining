b57fab51ba944c99749b84bdaed89fd5df068f9b
plflynn <flynn.patrick@hotmail.com>
2018-10-03 14:54:57 +0100

    customised portfolios routes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -45,5 +45,6 @@ class PortfoliosController < ApplicationController
     @portfolio_item.destroy
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
+    end
   end
 end
