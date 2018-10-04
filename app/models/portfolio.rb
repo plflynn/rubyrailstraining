0676f691f00d9d0335c06377314defced5b162c6
plflynn <flynn.patrick@hotmail.com>
2018-10-04 14:30:17 +0100

    set custom defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,11 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails")}
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "https://picsum.photos/200/300"
+    self.thumb_image ||= "https://picsum.photos/200/300"
+  end
 end
